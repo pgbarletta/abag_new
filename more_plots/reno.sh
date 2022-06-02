9dc8ea089f627fa4701a7a1398c56ec0df7c92f8
pgbarletta <pbarletta@gmail.com>
2022-06-02 20:06:37 +0200

    my own plots

---
+++ more_plots/reno.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp SI_hydro.jpg     															renaming/fig2.jpg
+# cp fix_DSSP_fig3d.jpg     														renaming/fig5d.jpg
+# cp 4xx1_hydro.jpg     															renaming/fig6a.jpg
+# cp hydro_carbons_in_biggest_cdrs_fig6b.jpg     									renaming/fig6b.jpg
+# cp hydro_cdrs_fig6c.jpg     													renaming/fig6c.jpg
+# cp SI_resname_of_the_carbons_from_the_biggest_hydrophobic_cluster_by_atom.jpg   renaming/fig6d.jpg
+# cp SI_polar.jpg 																renaming/fig8.jpg
+# cp polar_fig_8b.jpg     														renaming/fig.jpg
+# cp polar_cdrs_fig8d.jpg      													renaming/fig.jpg
+# cp hbond_salt_wat_fig8c.jpg     												renaming/fig.jpg
+# cp Figura-polar-bonds-v2.jpg          											renaming/fig8.jpg
+#cp fix_Figura-Tyr-Ser.jpg          													renaming/fig10.jpg
---
+++ more_plots/sreno.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# cp SI_second_biggest_cluster_over_biggest_atm.jpg            srenaming/figs5b.jpg
+# cp SI_size_of_the_largest_cluster_of_each_PDB_by_res.jpg     srenaming/figs5c.jpg
+# cp SI_number_of_cdrs_in_the_biggest_cluster.jpg              srenaming/figs5d.jpg
+# cp SI_cdrs_combinations_in_biggest_cluster_2.jpg             srenaming/figs6.jpg
+# cp SI_DSSP.jpg                                               srenaming/figs7.jpg
+# cp hydro_aa_fig6e.jpg                                        srenaming/figs8.jpg
+# cp SI_polar_interactions.jpg                                 srenaming/figs11.jpg
+# cp SI_fixwat_only.jpg                                        srenaming/figs12.jpg
+# cp SI_phe_interactions_ratio_inter.jpg                       srenaming/figs14a.jpg
+cp SI_venn_cdr_4.jpg                                           srenaming/figs15.jpg
